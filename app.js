648b4278a309bda74fa324014bcc0bd53f49dca1
icedike <icedike@gmail.com>
2020-08-14 18:26:09 +0800

    feat: add generate random code function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generateUrl = require('./generateUrl')
+const Url = require("./models/url")
 
 const app = express()
 const port = 3000
@@ -7,9 +10,19 @@ require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/url', async (req, res) => {
+  const originalUrl = req.body.originalUrl
+  const shortUrl = await generateUrl()
+  console.log(originalUrl, shortUrl)
+  Url.create({ originalUrl: originalUrl, shortUrl: shortUrl })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
---
+++ generateUrl.js
@@ -0,0 +1,36 @@
+
+const Url = require('./models/url')
+
+function generateCode() {
+  let urlCode = ''
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const code = (lowerCaseLetters + upperCaseLetters + numbers).split('')
+
+  for (let i = 0; i < 5; i++) {
+    urlCode += code[Math.floor(Math.random() * code.length)]
+  }
+
+  return urlCode
+}
+
+async function generateUrl() {
+  let urlExist = true
+  let urlCode = ''
+  console.log('start')
+  while (urlExist) {
+    urlCode = await generateCode()
+    await Url.exists({ shortUrl: urlCode })
+      .then(function (result) {
+        if (!result) {
+          urlExist = false
+        }
+      })
+      .catch(error => console.log(error))
+  }
+
+  return urlCode
+}
+
+module.exports = generateUrl
